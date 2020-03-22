9b0cc907107f714f1eaee8536955db4624bb9e79
Jain-Sanchit <Jainsanchit157@gmail.com>
2020-03-22 12:00:35 +0530

    port update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 
 const app = express();
@@ -7,54 +6,44 @@ const hbs = require("hbs");
 
 const path = require("path");
 
-const covid = require('novelcovid');
+const covid = require("novelcovid");
 const fetch = require("node-fetch");
 
-app.set('view engine', 'hbs')
+app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 
-
-;
-
-
-
-
 app.get("/", async (req, res) => {
-    let data = await fetch('https://corona.lmao.ninja/all')
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            var d = new Date(); // for now
-            d.getHours(); // => 9
-            d.getMinutes(); // =>  30
-            d.getSeconds();
-            // Work with JSON data here
-            res.render("home", { data,d });
-            //console.log(data)
-        })
-        .catch(err => {
-            // Do something for an error here
-        })
-        
-        
-  
+  let data = await fetch("https://corona.lmao.ninja/all")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      var d = new Date(); // for now
+      d.getHours(); // => 9
+      d.getMinutes(); // =>  30
+      d.getSeconds();
+      // Work with JSON data here
+      res.render("home", { data, d });
+      //console.log(data)
+    })
+    .catch(err => {
+      // Do something for an error here
+    });
 });
 
-app.get('/countryWise',async(req,res)=>{
-    let data = await fetch('https://corona.lmao.ninja/countries')
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            
-            // Work with JSON data here
-           // data = JSON.stringify(data)
-            res.render("countryWise", { data:data});
-            //console.log(data)
-        })
-        .catch(err => {
-            // Do something for an error here
-        })
-})
-app.listen(process.env.PORT || 8080);
+app.get("/countryWise", async (req, res) => {
+  let data = await fetch("https://corona.lmao.ninja/countries")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      // Work with JSON data here
+      // data = JSON.stringify(data)
+      res.render("countryWise", { data: data });
+      //console.log(data)
+    })
+    .catch(err => {
+      // Do something for an error here
+    });
+});
+app.listen(process.env.PORT || 3000, process.env.IP);
