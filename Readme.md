56a4948d4e5e13a8ffd0cc23e9cf77587e7bf3fa
Jain-Sanchit <Jainsanchit157@gmail.com>
2020-10-01 12:06:19 +0530

    a lot of changes to make it open source

--- Readme.md
+++ Readme.md
@@ -1,17 +1,72 @@
-# LiveCoronaStats
+
+ <h1 align="center">
+LiveCoronaStats
+</h1>
+
 
 Shows The live figures for corona in the whole world and country wise too.
 Shows the latest figures of USA state wise and INDIA state wise .
 
-Live on http://coronacasecount.live/
+## 🚀 Demo 
+Try the tool: [GitHub Profile README Generator](http://coronacasecount.live/)
 
-To run on your local device
+## 🧐 Features
 
-- Clone the repo in your device.
-- run "nmp i"
-- run " node app.js"
-- then open localhost:3000/ on any web browser.
+- ** World wide total figures ** 
+- ** Figures of today and yesterday **
+- ** Recovery figures and death figures **
+- ** India state wise figures **
+- ** USA state wise figures **
+- ** Country wise figures of current day and previous day**
+- ** Some general FAQ and precautions **
 
-If you want to contribute you are most welcomed just raise an issue or make a pull request.
 
-Bases on API via Covid19india Api and novelcovid .
+## 🛠️ Installation Steps
+
+1. Clone the repository
+
+```bash
+git clone https://github.com/Jain-Sanchit/LiveCoronaNumbers.git
+```
+
+2. Change the working directory
+
+```bash
+cd LiveCoronaNumbers
+```
+
+3. Install dependencies
+
+```bash
+npm install
+```
+
+4. Run the app
+
+```bash
+node app.js
+```
+5. Check on localhost 
+
+```bash
+http://localhost:3000/
+```
+
+🌟 You are all set!
+
+
+## 🍰 Contributing
+
+Please contribute using [GitHub Flow](https://guides.github.com/introduction/flow). Create a branch, add commits, and [open a pull request](https://github.com/Jain-Sanchit/LiveCoronaNumbers/compare).
+
+
+## 💻 Built with
+- [Nodejs]
+- [Handlebars] for views
+- Bases on API via Covid19india Api and novelcovid .
+
+<hr>
+<p align="center">
+Developed with ❤️ in India 🇮🇳 
+</p>
+
--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const app = express();
 
 const hbs = require("hbs");
 
-const https=require('https')
-
 const path = require("path");
 
-const covid = require("novelcovid");
 const fetch = require("node-fetch");
-var count=23591;
+
+var count=0;
+
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,31 +21,29 @@ hbs.registerPartials(__dirname + "/partials");
 
 app.get("/", async (req, res) => {
     count++;
-  let data = await fetch("https://corona.lmao.ninja/v2/all")
+  await fetch("https://corona.lmao.ninja/v2/all")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-        //console.log(count);
         
       var d = new Date(); // for now
-      d.getHours(); // => 9
-      d.getMinutes(); // =>  30
+      d.getHours(); //  hours
+      d.getMinutes(); // minutes
       d.getSeconds();
-      // Work with JSON data here
+
       var x = data.recovered + data.deaths;
-      //console.log(x);
 
       res.render("home", { data, d, x ,count});
-      //console.log(data)
     })
     .catch((err) => {
       // Do something for an error here
+      console.log(err);
     });
 });
 
 app.get("/care", async (req, res) => {
-  let data = await fetch("https://api.covid19india.org/website_data.json")
+  await fetch("https://api.covid19india.org/website_data.json")
     .then((response) => {
       return response.json();
     })
@@ -58,7 +55,7 @@ app.get("/care", async (req, res) => {
     .catch((err) => {});
 });
 app.get("/faq", async (req, res) => {
-  let data = await fetch("https://api.covid19india.org/website_data.json")
+  await fetch("https://api.covid19india.org/website_data.json")
     .then((response) => {
       return response.json();
     })
@@ -72,7 +69,7 @@ app.get("/faq", async (req, res) => {
     .catch((err) => {});
 });
 app.get("/India", async (req, res) => {
-  let data = await fetch("https://api.covid19india.org/data.json")
+    await fetch("https://api.covid19india.org/data.json")
     .then((response) => {
       return response.json();
     })
@@ -88,68 +85,52 @@ app.get("/India", async (req, res) => {
 });
 
 app.get("/yesterday", async (req, res) => {
-  let data = await fetch("https://corona.lmao.ninja/v2/all?yesterday=true")
+  await fetch("https://corona.lmao.ninja/v2/all?yesterday=true")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       var d = new Date(); // Today!
       d.setDate(d.getDate() - 1);
-
       var x = data.recovered + data.deaths;
-      // Work with JSON data here
       res.render("Yeshome", { data, d, x });
-      //console.log(data)
     })
     .catch((err) => {
       // Do something for an error here
     });
 });
 app.get("/countryWise", async (req, res) => {
-  let data = await fetch("https://corona.lmao.ninja/v2/countries?sort=cases")
+  await fetch("https://corona.lmao.ninja/v2/countries?sort=cases")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      //data.sort((a, b) => parseFloat(b.cases) - parseFloat(a.cases));
-      // Work with JSON data here
-      // data = JSON.stringify(data)
-      //console.log(data);
-
       res.render("countryWise", { data: data });
-      //console.log(data)
     })
     .catch((err) => {
       // Do something for an error here
     });
 });
 app.get("/countryWiseYesterday", async (req, res) => {
-  let data = await fetch("https://corona.lmao.ninja/v2/countries?yesterday=true&sort=cases")
+  await fetch("https://corona.lmao.ninja/v2/countries?yesterday=true&sort=cases")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      //data.sort((a, b) => parseFloat(b.cases) - parseFloat(a.cases));
-      // Work with JSON data here
-      // data = JSON.stringify(data)
       res.render("countryYesterday", { data: data });
-      //console.log(data)
     })
     .catch((err) => {
       // Do something for an error here
     });
 });
 app.get("/USAstates", async (req, res) => {
-  let data = await fetch("https://corona.lmao.ninja/v2/states")
+    await fetch("https://corona.lmao.ninja/v2/states")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       data.sort((a, b) => parseFloat(b.cases) - parseFloat(a.cases));
-      // Work with JSON data here
-      // data = JSON.stringify(data)
       res.render("stateWise", { data: data });
-      //console.log(data)
     })
     .catch((err) => {
       // Do something for an error here
